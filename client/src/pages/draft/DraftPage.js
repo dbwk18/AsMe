4eab63a93837a9ed3db1e588b6ca5ea2faefc385
dbwk18 <dbwk18@kaist.ac.kr>
2022-06-24 01:41:40 +0900

    design: writing box

--- client/src/pages/draft/DraftPage.js
+++ client/src/pages/draft/DraftPage.js
@@ -2,7 +2,10 @@ import {React, useState} from 'react';
 import { Grid, Box, Typography, Stack, Divider, List, ListItem, ListItemText, ListSubheader, Container } from '@mui/material';
 import { Editor } from 'react-draft-wysiwyg';
 import { EditorState } from 'draft-js';
-import SearchIcon from '../../assets/images/search.png'
+import TrashIcon from '../../assets/images/trashcan.png'
+import CloseBtn from '../../assets/images/closeBtn.png'
+import ProfileImg from '../../assets/images/profileImg.png'
+import defaultImg from '../../assets/images/WhitePaper2.png'
 
 import './DraftPage.css'
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
@@ -13,10 +16,6 @@ function DraftPage() {
     const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
     const [toolBtn, setToolBtn] = useState(null);
-    // const [previewTool, setPreviewTool] = useState(false);
-    // const [searchTool, setSearchTool] = useState(false);
-    // const [writingTool, setWritingTool] = useState(false);
-
     const [textQuery, setTextQuery] = useState('flying dog')
 
     const onEditorStateChange = (editorState) => {
@@ -48,8 +47,8 @@ function DraftPage() {
             <div className="draft-wrapper" position="absolute">
                 <div className="navigate-button">
                     <button className={`draftBtn ${toolBtn == "preview" ? "previewOn" : ""}`} onClick={()=>{setToolBtn("preview");}}>미리<br/>보기</button>
-                    <button className={`draftBtn ${toolBtn == "writing" ? "writeOn" : ""}`} onClick={()=>{setToolBtn("writing");}}>글감</button>
-                    <button className={`draftBtn ${toolBtn == "search" ? "searchOn" : ""}`} onClick={()=>{setToolBtn("search");}}><img width="25px" src={SearchIcon}></img></button>
+                    <button className={`draftBtn ${toolBtn == "writing" ? "writeOn" : ""}`} onClick={()=>{setToolBtn("writing");}}>글감<br/>보기</button>
+                    <button className={`draftBtn ${toolBtn == "trash" ? "trashOn" : ""}`} onClick={()=>{setToolBtn("trash");}}><img width="25px" src={TrashIcon}></img></button>
                 </div>
                 <Editor
                     // class for editor & toolbar
@@ -83,20 +82,95 @@ function DraftPage() {
                 : <></>
             }
             {
-                toolBtn == "search" 
+                toolBtn == "writing"  
                 ? (
                     <div className='popup-tool'>
-                        <div className='close-btn' onClick={()=>{setToolBtn(null);}}>X</div>
-                        <button onClick={()=>{get_image('flying dog');}}>검색</button>
+                        <div className='popup-header'>
+                            <div>
+                                <input className='popup-input' type="text" placeholder="글감을 검색하세요"/>
+                            </div>
+                            <div className='close-btn' onClick={()=>{setToolBtn(null);}}>
+                                <img width="30px" src={CloseBtn} /> 
+                            </div>
+                        </div>
+                        <div className='popup-header'>
+                            <div>
+                                <img width="70px" src={ProfileImg} />
+                            </div>
+                            <div className='popup-text'>
+                                000님의 휴지통 목록
+                            </div>
+                        </div>
+                        <div style={{position:"relative", top: "20px"}}>
+                            <div className='popup-content'>
+                                <img width="150px" src={defaultImg} />
+                                <div style={{display: "flex", flexDirection: "column", padding: "0px 45px"}}>
+                                    <div className='head'>
+                                        <div className='idx'>01</div>
+                                        <div className='date'>2022-04-21</div>
+                                    </div>
+                                    <div className='title'>운동하기</div>
+                                    <div style={{display: "flex"}}>
+                                        <div className='tag'>운동</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='popup-content'>
+                                <img width="150px" src={defaultImg} />
+                                <div style={{display: "flex", flexDirection: "column", padding: "0px 45px"}}>
+                                    <div className='head'>
+                                        <div className='idx'>02</div>
+                                        <div className='date'>2022-04-21</div>
+                                    </div>
+                                    <div className='title'>운동하기</div>
+                                    <div style={{display: "flex"}}>
+                                        <div className='tag'>김치찌개</div>
+                                        <div className='tag'>배고파요</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='popup-content'>
+                                <img width="150px" src={defaultImg} />
+                                <div style={{display: "flex", flexDirection: "column", padding: "0px 45px"}}>
+                                    <div className='head'>
+                                        <div className='idx'>01</div>
+                                        <div className='date'>2022-04-21</div>
+                                    </div>
+                                    <div className='title'>운동하기</div>
+                                    <div style={{display: "flex"}}>
+                                        <div className='tag'>운동</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='popup-content'>
+                                <img width="150px" src={defaultImg} />
+                                <div style={{display: "flex", flexDirection: "column", padding: "0px 45px"}}>
+                                    <div className='head'>
+                                        <div className='idx'>01</div>
+                                        <div className='date'>2022-04-21</div>
+                                    </div>
+                                    <div className='title'>운동하기</div>
+                                    <div style={{display: "flex"}}>
+                                        <div className='tag'>운동</div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+
                     </div>
                 )
                 : <></>
             }
             {
-                toolBtn == "writing"  
+                toolBtn == "trash" 
                 ? (
-                    <div className='popup-tool'>
-                        <div className='close-btn' onClick={()=>{setToolBtn(null);}}>X</div>
+                    <div className='toast-message'>
+                      
                     </div>
                 )
                 : <></>
