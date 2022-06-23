2e1d90caf59c9819b5bf44d9180cca3b52998d45
jodongha <jodongrami@gmail.com>
2022-06-24 00:35:00 +0900

    [#7] feat: Implement board api-response

--- server/api/src/routes/boardRouter.js
+++ server/api/src/routes/boardRouter.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 const boardController = require('../controllers/boardController');
 
 //글 목록
-router.get('/posts/:user_id', boardController.selectAllPost);
+router.get('/posts', boardController.selectAllPost);
 
 
 
