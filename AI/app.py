c95285bb9fb92b670ca466096244714054841fc4
함경윤 <201702005>
2022-06-23 20:17:05 +0900

    [#11] add.routemethods

--- AI/app.py
+++ AI/app.py
@@ -4,8 +4,11 @@ import requests
 # # 플라스크 웹 서버 객체 생성
 app = Flask(__name__) 
 
-@app.route('/dalle', methods=['POST'])
+@app.route('/dalle', methods=['GET', 'POST'])
 def dalle_2():
+    lists = request.args['file_name']
+    lists = lists.split(',')
+    data = []
     r = requests.post(
         "https://api.deepai.org/api/text2img",
         data={
