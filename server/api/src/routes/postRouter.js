4c47111a0e6d2f32211999a600334a576a05e86b
jodongha <jodongrami@gmail.com>
2022-06-24 03:33:39 +0900

    [#8] feat: Implement post api-combined

--- server/api/src/routes/postRouter.js
+++ server/api/src/routes/postRouter.js
@@ -1,19 +1,15 @@
 var router = require('express').Router();
 const postController = require('../controllers/postController');
 
-//개별 글 상세
-router.get('/post/:post_id', postController.selectPost);
-
-//개별 글 휴지통으로
-router.delete('/post/:post_id', postController.deletePost);
+//글 목록
+router.get('/posts', postController.selectAllPost);
 
 //글 생성
 router.post('/post', postController.insertPost);
 
+//개별 글 휴지통으로
+router.delete('/post/:post_id', postController.deletePost);
 
-
-// 글 업데이트
-// router.put('/:post_uid', postController.updatePost);
 
 
 
